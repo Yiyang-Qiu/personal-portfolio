85b33f65c094a34e3920189e463e5e7cc92439a8
Yiyang-Qiu <909842568qyy@gmail.com>
2024-08-28 00:42:40 -0500

    update navbar and added logo img/ name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
 import navIcon3 from '../assets/img/nav-icon3.svg'
+import logo from '../assets/img/Logo.png'
 
 //nav bar
 export const NavBar = () => {
@@ -32,7 +33,8 @@ export const NavBar = () => {
         <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
         <Container>
             <Navbar.Brand href="#home">
-                <img src={''} alt="Logo" />
+                {/* /<img src={logo} alt="Logo" /> */}
+                <label>Yiyang Qiu</label>
             </Navbar.Brand>
             <Navbar.Toggle aria-control = "basic-navbar-nav">
                 <span className="navbar-toggler-icon"></span>
@@ -42,13 +44,13 @@ export const NavBar = () => {
                 <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('home')}>Home</Nav.Link>
                 <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('skills')}>Skills</Nav.Link>
                 <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('projects')}>Projects</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item> 
                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item> 
                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
+                </NavDropdown> */}
             </Nav>
             <span className="navbar-text">
                 <div className="social-icon">
